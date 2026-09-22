3869f29049726a61d64aa11c82ebb4ed0b0a9c47
Faruq Olasupo <143729100+faruqlas@users.noreply.github.com>
2026-09-22 14:43:51 +0100

    Add files via upload

---
+++ element.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Script to look up element details by atomic number, symbol, or name
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+ELEMENT_INFO=$($PSQL "
+SELECT
+  e.atomic_number,
+  e.name,
+  e.symbol,
+  p.atomic_mass,
+  p.melting_point_celsius,
+  p.boiling_point_celsius,
+  t.type
+FROM elements e
+JOIN properties p
+  ON e.atomic_number = p.atomic_number
+JOIN types t
+  ON p.type_id = t.type_id
+WHERE e.atomic_number::TEXT = '$1'
+   OR e.symbol = '$1'
+   OR e.name = '$1';
+")
+
+if [[ -z $ELEMENT_INFO ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+IFS='|' read ATOMIC_NUMBER NAME SYMBOL ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE <<< "$ELEMENT_INFO"
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
